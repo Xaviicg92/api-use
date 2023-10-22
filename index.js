01cdcbdb683f9e9ea81ac3758e671f8b20679f2a
Javier Cambón <cg_javi@hotmail.com>
2023-10-22 17:41:38 +0200

    separate js from html

---
+++ index.js
@@ -0,0 +1,68 @@
+(function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: 828200545756072,
+      xfbml: true,
+      version: "v18.0",
+    });
+  };
+
+  function statusChangeCallback(response) {
+    // Called with the results from FB.getLoginStatus().
+    console.log("statusChangeCallback");
+    console.log(response); // The current login status of the person.
+    if (response.status === "connected") {
+      // Logged into your webpage and Facebook.
+      testAPI();
+    } else {
+      // Not logged into your webpage or we are unable to tell.
+      document.getElementById("status").innerHTML =
+        "Please log " + "into this webpage.";
+    }
+  }
+
+  function checkLoginState() {
+    // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {
+      // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+  function testAPI() {
+    // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log("Welcome!  Fetching your information.... ");
+    FB.api("/me", function (response) {
+      console.log("Successful login for: " + response.name);
+      document.getElementById("status").innerHTML =
+        "Thanks for logging in, " + response.name + "!";
+    });
+  }
+
+  function addPermisions() {
+    FB.login(
+      function (response) {
+        if (response.authResponse) {
+          console.log("El usuario autorizó la aplicación.");
+
+          // Aquí puedes hacer llamadas a la API de Graph para obtener información del usuario
+        } else {
+          console.log(
+            "El usuario canceló el inicio de sesión o no autorizó la aplicación."
+          );
+        }
+      },
+      { scope: "public_profile,user_followers" }
+    ); // Aquí se solicitan los permisos necesarios
+  }
