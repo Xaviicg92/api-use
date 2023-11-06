9f32d5586e0077b4b080cf45d14729657e90a82d
Javier Cambón <cg_javi@hotmail.com>
2023-11-06 12:34:39 +0100

    try to add permissions

--- index.js
+++ index.js
@@ -43,21 +43,14 @@
 
   function testAPI() {
     // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-    console.log("Welcome!  Fetching your information.... ");
-    FB.api("/me", function (response) {
-      console.log(response);
-      document.getElementById("status").innerHTML =
-        "Thanks for logging in, " + response.name + "!";
-    });
-  }
-
-  function getFollowers() {
-    FB.api('/me/friends', function(response) {
-      if (response && !response.error) {
-        var followers = response.data;
-        console.log(followers); // Puedes hacer lo que quieras con la lista de seguidores aquí
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
       } else {
-        console.log('Error al obtener seguidores');
+        console.log('User cancelled login or did not fully authorize.');
       }
     });
   }
